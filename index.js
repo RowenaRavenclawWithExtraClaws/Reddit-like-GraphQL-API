84db548363a09578a63e30a0e101b54fcdb33959
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-07-04 14:39:25 +0200

    setup the graphql server on top of express

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+const { schema } = require('./schema');
+const { root } = require('./root');
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(4000, () =>
+    console.log('Running a GraphQL API server at localhost:4000/graphql'));

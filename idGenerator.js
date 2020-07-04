0c45134025c5b955588c7874e2a5863653f5b37b
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-07-04 23:56:43 +0200

    implementing user id geneeration

--- idGenerator.js
+++ idGenerator.js
@@ -1,7 +1,29 @@
 'use strict'
 
 let generateRandomUserID = () => {
+    let generatedID = '';
+    let coinFlip = 0;
+    let counter = 0;
+    let char = '';
+    let nums = '0123456789';
+    let chars = 'abcdefghijklmnopqrstuvwxyz';
 
+    while (counter < 9) {
+        coinFlip = Math.floor(Math.random() * 2);
+
+        if (coinFlip == 0) {
+            char = nums[Math.floor(Math.random() * nums.length)];
+            generatedID += char;
+        }
+        else {
+            char = chars[Math.floor(Math.random() * chars.length)];
+            generatedID += char;
+        }
+
+        counter++;
+    }
+
+    return generatedID;
 }
 
 let generateRandomCommunityID = () => {
@@ -11,3 +33,5 @@ let generateRandomCommunityID = () => {
 let generateRandomPostID = () => {
 
 }
+
+exports.generateRandomUserID = generateRandomUserID;
